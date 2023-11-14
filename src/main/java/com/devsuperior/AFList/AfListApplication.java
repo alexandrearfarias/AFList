012add8d5b00ff87b028c91c98ed24f19eb22bd2
Alexandre <alexandrearfarias@gmail.com>
2023-11-14 10:23:37 -0300

    projeto criado

---
+++ src/main/java/com/devsuperior/AFList/AfListApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.AFList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/AFList/AfListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.AFList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
