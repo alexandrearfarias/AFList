d6506a0f89798cc1ac3a1b145a49b3edb0d66ba0
Alexandre <alexandrearfarias@gmail.com>
2023-11-19 10:45:32 -0300

    configuracao de rota e servico

---
+++ src/main/java/com/devsuperior/AFList/dto/GameDTO.java
@@ -0,0 +1,97 @@
+package com.devsuperior.AFList.dto;
+
+import org.springframework.beans.BeanUtils;
+
+import com.devsuperior.AFList.entidades.Game;
+
+public class GameDTO {
+	private long id;
+	private String titulo;
+	private Integer ano;
+	private String genero;
+	private String plataforma;
+	private String imgUrl;
+	private String descricaoCurta;
+	private String descricaoLonga;
+	private double pontuacao;
+	
+	public GameDTO() {}
+	
+	public GameDTO(Game entity) {
+		BeanUtils.copyProperties(entity, this);
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public String getDescricaoCurta() {
+		return descricaoCurta;
+	}
+
+	public void setDescricaoCurta(String descricaoCurta) {
+		this.descricaoCurta = descricaoCurta;
+	}
+
+	public String getDescricaoLonga() {
+		return descricaoLonga;
+	}
+
+	public void setDescricaoLonga(String descricaoLonga) {
+		this.descricaoLonga = descricaoLonga;
+	}
+
+	public double getPontuacao() {
+		return pontuacao;
+	}
+
+	public void setPontuacao(double pontuacao) {
+		this.pontuacao = pontuacao;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	
+	
+}
--- src/main/java/com/devsuperior/AFList/services/GameService.java
+++ src/main/java/com/devsuperior/AFList/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.AFList.dto.GameDTO;
 import com.devsuperior.AFList.dto.GameMinDTO;
 import com.devsuperior.AFList.entidades.Game;
 import com.devsuperior.AFList.repositories.GameRepository;
@@ -15,8 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly= true)
 	public List<GameMinDTO> findAll(){
 		var result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly= true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
 	}
 }
